e88117d2516b4d73d8e0de7c0b7f71389c5e3a79
jenssss <37403847+jenssss@users.noreply.github.com>
2019-01-10 01:09:11 +0900

    Added animation class to matplotlib managers

--- runana/matplotlib_managers.py
+++ runana/matplotlib_managers.py
@@ -13,13 +13,12 @@ from matplotlib.figure import Figure
 from runana.run import makedir
 
 
-class SvgFiles(object):
-    """ Keeps track of svg files in a directory
+class IndivFiles(object):
+    """ Keeps track of png or svg files in a directory
     """
     def __init__(self, outfile, *args, **kwargs):
         basedir, file_ext = splitext(outfile)
         basedir = abspath(pjoin(getcwd(), basedir))
-        print("basedir", basedir)
         makedir(basedir)
         self.outfile = outfile
         self.basedir = basedir
@@ -27,18 +26,26 @@ class SvgFiles(object):
         self.args = args
         self.kwargs = kwargs
         self.page = 1
+        if self.file_ext == ".png":
+            self.canvas_func = FigureCanvas
+        elif self.file_ext == ".svg":
+            self.canvas_func = FigureCanvasSVG
+        else:
+            raise ValueError("File extension {} is not known".format(self.file_ext))
 
     def savefig(self, figure, **kwargs):
         try:
-            fname = pjoin(self.basedir, "{}.svg".format(self.page))
+            fname = pjoin(self.basedir, "{}{}".format(self.page,
+                                                       self.file_ext))
             if self.page == 1:
                 fname = pjoin(split(self.basedir)[0], self.outfile)
             elif self.page == 2:
                 rename(pjoin(split(self.basedir)[0], self.outfile),
-                       pjoin(self.basedir, "{}.svg".format(1)))
+                       pjoin(self.basedir, "{}{}".format(1,
+                                                          self.file_ext)))
             orig_canvas = figure.canvas
-            figure.canvas = FigureCanvasSVG(figure)
-            figure.savefig(fname, format="svg", **kwargs)
+            figure.canvas = self.canvas_func(figure)
+            figure.savefig(fname, format=self.file_ext[1:], **kwargs)
             self.page += 1
         finally:
             figure.canvas = orig_canvas
@@ -49,6 +56,81 @@ class SvgFiles(object):
     def close(self):
         """ No clean up needed """
         pass
+
+
+class webmMovie(IndivFiles):
+
+    def __init__(self, outfile, *args, **kwargs):
+        self.outfile_video = outfile
+        self.bitrate = kwargs.pop("bitrate", "1M")
+        self.reverse = kwargs.pop("reverse", True)
+        outfile = outfile.replace(".webm", ".png")
+        super(webmMovie, self).__init__(outfile, *args, **kwargs)
+
+    def print_args(self, args, fname, mode="w"):
+        with open(pjoin(self.basedir, fname), mode) as file_:
+            file_.write(" ".join(args+["\n"]))
+
+    def close(self):
+        from subprocess import call
+        outfile_video = self.outfile_video
+        options = ["-y", "-c:v", "libvpx-vp9", "-b:v", self.bitrate]
+        args = ["ffmpeg"]
+        args.extend(["-i", pjoin(self.basedir, "%d.png")])
+        # lines = []
+        # filenames = []
+        nfiles = self.page-1
+        for i in range(nfiles):
+            filename = pjoin(self.basedir, "{}.png".format(i+1))
+            filename2 = pjoin(self.basedir, "{}.png".format(2*nfiles-i))
+            from os import remove
+            from shutil import copy
+            from runana.run import ignored
+            with ignored(OSError):
+                remove(filename2)
+            copy(filename, filename2)
+        # filenames = []
+        # for i in range(self.page-1):
+        #     filename = pjoin(self.basedir, "{}.png".format(i+1))
+        #     filenames.append(filename)
+        #     lines.append("file '{}'\n".format(filename))
+        # if self.reverse:
+        #     filenames += reversed(filenames)
+        # input_files = pjoin(self.basedir, "input_files.txt")
+        # with open(input_files, "w") as file_:
+        #     file_.write("".join(lines))
+        #     if self.reverse:
+        #         file_.write("".join(reversed(lines)))
+        # args.extend(["-f", "concat",  "-i", input_files])
+        # nested_list = [["-i", filename] for filename in filenames]
+        # args.extend([arg for filename in filenames
+        #              for arg in ["-i", filename]])
+        # args.extend([item for sub in nested_list for item in sub])
+        args.extend(options)
+        args.append(outfile_video)
+        call(args)
+        self.print_args(args, "ffmpeg_args.txt")
+        # with open(pjoin(self.basedir, "ffmpeg_args.txt"), "w") as file_:
+        #     file_.write(" ".join(args+["\n"]))
+        # if self.reverse:
+        #     reverse_fname = "reverse.webm"
+        #     args = ["ffmpeg", "-i", outfile_video,
+        #             "-vf", "reverse"]
+        #     args.extend(options)
+        #     args.append(reverse_fname)
+        #     call(args)
+        #     self.print_args(args, "ffmpeg_args.txt", mode="aw")
+        #     video_list = pjoin(self.basedir, "video_list.txt")
+        #     self.print_args(["file", outfile_video], video_list, mode="w")
+        #     self.print_args(["file", reverse_fname], video_list, mode="aw")
+        #     args = ["ffmpeg", "-f", "concat",
+        #             "-i", video_list,
+        #             reverse_fname]
+        #     # args.extend(options)
+        #     args.extend(options)
+        #     args.append("reverse_combined.webm")
+        #     call(args)
+        #     self.print_args(args, "ffmpeg_args.txt", mode="aw")
 
 
 class plot_manager(object):
@@ -67,8 +149,10 @@ class plot_manager(object):
         _, file_ext = splitext(self.outfile)
         if file_ext == ".pdf":
             self.pp = PdfPages(self.outfile, *self.args, **self.kwargs)
-        elif file_ext == ".svg":
-            self.pp = SvgFiles(self.outfile, *self.args, **self.kwargs)
+        elif file_ext == ".svg" or file_ext == ".png":
+            self.pp = IndivFiles(self.outfile, *self.args, **self.kwargs)
+        elif file_ext == ".webm":
+            self.pp = webmMovie(self.outfile, *self.args, **self.kwargs)
         else:
             raise ValueError("Unknown file extension {}".format(file_ext))
         return self.pp
