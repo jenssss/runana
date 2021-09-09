c8962f3a2f564820046a2875ef4c8daf55321824
jenssss <37403847+jenssss@users.noreply.github.com>
2021-09-09 17:37:59 +0900

    Trying dev_template for pypi test build

--- setup.py
+++ setup.py
@@ -9,13 +9,15 @@ VERSION = "0.1.5"
 
 setup(
     name="runana",
-    version=VERSION,
+    # version=VERSION,
     author="Jens Svensmark",
     author_email="jenssss@uec.ac.jp",
     version_config={
+        "dev_template": "{tag}.dev{ccount}",
+        # "dirty_template": "{tag}.dev{ccount}",
         # "dev_template": VERSION+".dev{ccount}",
         # "starting_version": VERSION,
-        # "template": VERSION+"lol{tag}",
+        # "template": VERSION+"lol{tag}+dev{ccount}",
         },
     setup_requires=["setuptools-git-versioning"],
     # version_config={
