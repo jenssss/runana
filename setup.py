7aed7380a68b60550f03bd86b2b366fe3a4f67ca
jenssss <37403847+jenssss@users.noreply.github.com>
2021-09-09 17:42:25 +0900

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-VERSION = "0.1.5"
+# VERSION = "0.1.5"
 
 setup(
     name="runana",
